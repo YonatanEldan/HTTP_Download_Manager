5cd99ca2562f80606f6c2b598142f3eaaaca3894
JonyEldan <eldan.yonatan@gmail.com>
2020-01-17 17:53:41 +0200

    finished worker(hopefully)

--- src/modules/Worker.java
+++ src/modules/Worker.java
@@ -1,5 +1,4 @@
 package modules;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,20 +19,27 @@ public class Worker implements Runnable {
     int sizeOfChunk;
     ArrayBlockingQueue<DataChunk> queue;
 
+    //Testing paramaters
+    int iteration = 1;
+
     public Worker(long firstByteIndex, long lastByteIndex, String url, int sizeOfChunk, ArrayBlockingQueue<DataChunk> queue){
         this.firstByteIndex = firstByteIndex;
         this.lastByteIndex = lastByteIndex;
         this.url = url;
         this.sizeOfChunk = sizeOfChunk;
         this.queue = queue;
+        this.dataChunk = new DataChunk(this.firstByteIndex, this.firstByteIndex + this.sizeOfChunk);
     }
 
     @Override
     public void run() {
         while(this.firstByteIndex + this.sizeOfChunk < this.lastByteIndex) {
+            System.out.println("number of iteration" + iteration + "firstByteIndex" + this.firstByteIndex + "lastByteIndex" + this.lastByteIndex );
+            this.dataChunk = new DataChunk(this.firstByteIndex, this.firstByteIndex + sizeOfChunk);
             readFromServer();
             writeToQueue();
             this.firstByteIndex += this.sizeOfChunk;
+            iteration++;
         }
 
 
@@ -48,10 +54,10 @@ public class Worker implements Runnable {
             URL url = new URL(this.url);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            //
             con.setRequestProperty("Range", "bytes="+ dataChunk.getFirstByteIndex()+"-" + dataChunk.getlastByteIndex());
             InputStream inputStream = con.getInputStream();
             byte[] buffer = new byte[this.dataChunk.size];
+            System.out.println("Iteration" + iteration + "http status" + con.getResponseCode());
             // read stream data into buffer
             inputStream.read(buffer);
             this.dataChunk.data = buffer;
@@ -69,7 +75,6 @@ public class Worker implements Runnable {
             e.printStackTrace();
             System.out.println("error in writeToQueue");
         }
-      //  this.dataChunk.data = [];
 
     }
 }
