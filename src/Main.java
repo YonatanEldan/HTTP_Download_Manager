89d0de9c2781bc85cdee45726450b08d0cb07c07
Nadav Dor <nadavdor274@gmail.com>
2020-01-17 12:26:43 +0200

    init worker object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Yonatan!");
+       // get input from the user
+
+
+       // init worker
+
+       // connect to server
+
+       //loop: read all the remote file by chunks.
+
+
     }
 }
