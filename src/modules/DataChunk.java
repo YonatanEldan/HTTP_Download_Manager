e95f0c9a2e9f90d6c14cf889062b6d5294b80e71
JonyEldan <eldan.yonatan@gmail.com>
2020-01-17 17:27:50 +0200

    **ALMOST** finished worker

--- src/modules/DataChunk.java
+++ src/modules/DataChunk.java
@@ -1,13 +1,26 @@
 package modules;
 
+import javax.xml.crypto.Data;
+
 public class DataChunk{
 
-        long firstByteIndex, lastByteIndex;
-        Byte[] data;
+        private long firstByteIndex, lastByteIndex;
+        byte[] data;
+        int size;
 
         public DataChunk(long startIndex, long endIndexx){
             this.firstByteIndex = firstByteIndex;
             this.lastByteIndex = lastByteIndex;
-            this.data = new Byte[(int)(lastByteIndex-firstByteIndex)];
+            this.data = new byte[(int)(lastByteIndex-firstByteIndex)];
+            this.size = (int)(lastByteIndex - firstByteIndex);
         }
+
+        public long getFirstByteIndex() {
+            return this.firstByteIndex;
+        }
+
+        public long getlastByteIndex() {
+            return this.lastByteIndex;
+        }
+
 }
--- src/modules/Worker.java
+++ src/modules/Worker.java
@@ -1,12 +1,14 @@
 package modules;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Worker implements Runnable {
@@ -15,31 +17,25 @@ public class Worker implements Runnable {
     long firstByteIndex, lastByteIndex;
     BufferedReader reader = null;
     PrintWriter writer = null;
+    int sizeOfChunk;
+    ArrayBlockingQueue<DataChunk> queue;
 
-    public Worker(long firstByteIndex, long lastByteIndex, String url){
+    public Worker(long firstByteIndex, long lastByteIndex, String url, int sizeOfChunk, ArrayBlockingQueue<DataChunk> queue){
         this.firstByteIndex = firstByteIndex;
         this.lastByteIndex = lastByteIndex;
         this.url = url;
+        this.sizeOfChunk = sizeOfChunk;
+        this.queue = queue;
     }
 
     @Override
     public void run() {
-        char c;
-        int i=0;
-        URL url = new URL("http://centos.activecloud.co.il/6.10/isos/x86_64/CentOS-6.10-x86_64-netinstall.iso");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Range", "bytes=0-1023");
-        InputStream is = con.getInputStream();
-        byte[] buffer = new byte[2048];
-        // read stream data into buffer
-        int data;
-        while ((data = is.read()) != -1) {
-            System.out.println(i);
-            i++;
+        while(this.firstByteIndex + this.sizeOfChunk <= this.lastByteIndex) {
+            readFromServer();
+            writeToQueue();
+            this.firstByteIndex += this.sizeOfChunk;
         }
-        //System.out.println(i);
-        is.close();
+
 
     }
 
@@ -48,10 +44,32 @@ public class Worker implements Runnable {
     }
 
     private void readFromServer(){
-
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            //
+            con.setRequestProperty("Range", "bytes="+ dataChunk.getFirstByteIndex()+"-" + dataChunk.getlastByteIndex());
+            InputStream inputStream = con.getInputStream();
+            byte[] buffer = new byte[this.dataChunk.size];
+            // read stream data into buffer
+            inputStream.read(buffer);
+            this.dataChunk.data = buffer;
+            inputStream.close();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("error in readFromServer");
+        }
     }
 
-    private void writeToQueue(BlockingQueue queue){
+    private void writeToQueue(){
+        try{
+            this.queue.put(this.dataChunk);
+          }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("error in writeToQueue");
+        }
+        this.dataChunk.data = [];
 
     }
 }
