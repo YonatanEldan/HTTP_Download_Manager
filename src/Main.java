f5a5160ad4d3c107a47168c218a168dbce1c662c
JonyEldan <eldan.yonatan@gmail.com>
2020-01-17 16:18:48 +0200

    added HTTP request

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import modules.Worker;
+import java.net.*;
+import  java.io.*;
+import java.util.*;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
        // get input from the user
         for (String s: args) {
@@ -11,10 +15,29 @@ public class Main {
 
 
         // init worker obj
-        Worker worker = new Worker();
+        //Worker worker = new Worker();
        // connect to server
 
        //loop: read all the remote file by chunks.
+
+        URL url = new URL("http://centos.activecloud.co.il");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Range", "bytes=0-1023");
+        con.setRequestProperty("Host", "6.10/isos/x86_64/CentOS-6.10-x86_64-netinstall.iso");
+        System.out.println(con.getResponseCode());
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+
+
+
 
     }
 }
