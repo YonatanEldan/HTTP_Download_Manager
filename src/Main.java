c395f396f9c3b42d890280296145aec436d3258f
JonyEldan <eldan.yonatan@gmail.com>
2020-01-17 16:54:10 +0200

    added HTTP request to worker

--- src/Main.java
+++ src/Main.java
@@ -20,24 +20,11 @@ public class Main {
 
        //loop: read all the remote file by chunks.
 
-        URL url = new URL("http://centos.activecloud.co.il");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Range", "bytes=0-1023");
-        con.setRequestProperty("Host", "6.10/isos/x86_64/CentOS-6.10-x86_64-netinstall.iso");
-        System.out.println(con.getResponseCode());
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
+    }
 
 
 
 
 
     }
-}
+
--- src/modules/Worker.java
+++ src/modules/Worker.java
@@ -1,8 +1,11 @@
 package modules;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 
@@ -21,6 +24,22 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
+        char c;
+        int i=0;
+        URL url = new URL("http://centos.activecloud.co.il/6.10/isos/x86_64/CentOS-6.10-x86_64-netinstall.iso");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Range", "bytes=0-1023");
+        InputStream is = con.getInputStream();
+        byte[] buffer = new byte[2048];
+        // read stream data into buffer
+        int data;
+        while ((data = is.read()) != -1) {
+            System.out.println(i);
+            i++;
+        }
+        //System.out.println(i);
+        is.close();
 
     }
 
