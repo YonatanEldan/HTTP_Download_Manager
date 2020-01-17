975446ccecb4379e6cc6148c083a3364b96beeaa
Nadav Dor <nadavdor274@gmail.com>
2020-01-17 13:09:30 +0200

    init dataChunk

--- src/modules/Worker.java
+++ src/modules/Worker.java
@@ -1,4 +1,21 @@
 package modules;
 
-public class Worker {
+import java.util.ArrayList;
+
+public class Worker implements Runnable {
+    ArrayList<Byte> dataChunk;
+    int maxSize;
+    long firstByteIndex;
+    long lastByteIndex;
+
+
+
+    @Override
+    public void run() {
+
+    }
+
+    private void connect(){
+
+    }
 }
