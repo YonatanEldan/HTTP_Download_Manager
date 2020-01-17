435bda929c7e6147b4d423ad9236fb2cb3889093
JonyEldan <eldan.yonatan@gmail.com>
2020-01-17 13:31:30 +0200

    added datachunk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import modules.Worker;
+
 public class Main {
 
     public static void main(String[] args) {
+
        // get input from the user
+        for (String s: args) {
+            System.out.println(s);
+        }
 
 
-       // init worker obj
-
+        // init worker obj
+        Worker worker = new Worker();
        // connect to server
 
        //loop: read all the remote file by chunks.
-
 
     }
 }
--- src/modules/DataChunk.java
+++ src/modules/DataChunk.java
@@ -1,4 +1,13 @@
 package modules;
 
-public class DataChunk {
+public class DataChunk{
+
+        long firstByteIndex, lastByteIndex;
+        Byte[] data;
+
+        public DataChunk(long startIndex, long endIndexx){
+            this.firstByteIndex = firstByteIndex;
+            this.lastByteIndex = lastByteIndex;
+            this.data = new Byte[(int)(lastByteIndex-firstByteIndex)];
+        }
 }
