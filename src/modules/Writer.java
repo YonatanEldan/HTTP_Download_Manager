81f753bab4f040edb5864d4ccdf2fbcb71e23db5
Nadav Dor <nadavdor274@gmail.com>
2020-01-18 11:54:44 +0200

    writer update

--- src/modules/Writer.java
+++ src/modules/Writer.java
@@ -1,4 +1,36 @@
 package modules;
 
-public class Writer {
+import jdk.jfr.DataAmount;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Writer implements Runnable{
+    ArrayBlockingQueue<DataChunk> queue;
+    RandomAccessFile file;
+    DataChunk dataChunk;
+
+    public Writer(ArrayBlockingQueue q, RandomAccessFile f){
+        this.queue = q;
+        this.file = f;
+    }
+
+
+    @Override
+    public void run() {
+
+        while(true){
+            try {
+                dataChunk = queue.take();
+                file.seek(dataChunk.getFirstByteIndex());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
