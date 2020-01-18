2b26331b11c543cf9fd9f9ecccec18b5be8ec3f4
JonyEldan <eldan.yonatan@gmail.com>
2020-01-18 12:14:16 +0200

    finished main for worker and writer testing

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import modules.*;
+import modules.Writer;
+
 import java.net.*;
 import  java.io.*;
 import java.util.*;
@@ -18,12 +20,21 @@ public class Main {
         // init blocking queue
         ArrayBlockingQueue<DataChunk> queue = new ArrayBlockingQueue<>(1000);
 
+
         // init worker obj
         String url = "http://centos.activecloud.co.il/6.10/isos/x86_64/CentOS-6.10-x86_64-netinstall.iso";
         Worker worker = new Worker(0, 240123904, url, 409600, queue);
         Thread T = new Thread(worker);
         T.run();
 
+        //init writer obj
+        RandomAccessFile raf = new RandomAccessFile("NewFileJava.iso", "rw");
+        Writer writer = new Writer(queue, raf);
+        write.run();
+        raf.close();
+
+
         System.out.println("the size of the queue is : " + queue.size());
+
     }
 }
