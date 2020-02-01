1baae6ca60f55951563f503334944bb0893f866d
Nadav Dor <nadavdor274@gmail.com>
2020-02-01 13:23:49 +0200

    final refactoring

--- src/IdcDm.java
+++ src/IdcDm.java
@@ -3,26 +3,22 @@ import  java.io.*;
 import java.util.*;
 
 
-public class Main {
+public class IdcDm {
 
     public static void main(String[] args) throws IOException {
         //In case we don't get an input for max num threads
         int maxNumOfThreads = 1;
+
         if(args.length==2) {
             maxNumOfThreads = Integer.parseInt(args[1]);
         }
         String[] servers = InputValidation.validate(args[0]);
+
         //init manager
         Manager manager = new Manager(servers, maxNumOfThreads);
         String resultMessage = manager.execute();
 
         System.out.println(resultMessage);
-
-
-        // run the tests and delete the new file
-        Tests.main(new String[]{});
-        System.out.println("deleting the new file and exit the program...");
-
     }
 
 }
